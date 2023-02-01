9014a8abbc1112353f751533d8551055679349b2
haritsah <haritsah@qlue.id>
2023-02-01 17:52:09 +0700

    reformat test commands

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cmake -DRUN_TESTS=ON -DCMAKE_BUILD_TYPE=Debug .. && make
-ctest --output-on-failure --progress && lcov --capture --directory . --output-file coverage.info
-
+ctest --output-on-failure --progress
+lcov --capture --directory . --output-file coverage.info
 mkdir -p /mnt/coverage && cp coverage.info /mnt/coverage/coverage.info
