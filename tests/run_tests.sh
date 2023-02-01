4fb07b8edbd53fe19f798d0c79a0e8082a46f419
haritsah <haritsah@qlue.id>
2023-02-01 17:43:45 +0700

    add scripts to run action and codecov

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,4 +2,5 @@
 
 cmake -DRUN_TESTS=ON -DCMAKE_BUILD_TYPE=Debug .. && make
 ctest --output-on-failure --progress && lcov --capture --directory . --output-file coverage.info
-lcov --remove coverage.info '/usr/*' --output-file coverage.info
+
+mkdir -p /mnt/coverage && cp coverage.info /mnt/coverage/coverage.info
