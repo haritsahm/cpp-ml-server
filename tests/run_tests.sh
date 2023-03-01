6765ab9be055e6f31df1b812e44e8c8971b1391f
haritsah <haritsah@qlue.id>
2023-03-01 19:24:06 +0700

    Disable triton in tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmake -DRUN_TESTS=ON -DCMAKE_BUILD_TYPE=Debug .. && make
+cmake -DRUN_TESTS=ON  -DENABLE_TRITON=OFF -DCMAKE_BUILD_TYPE=Debug .. && make
 ctest --output-on-failure --progress
 lcov --capture --directory . --output-file coverage.info
 mkdir -p /mnt/coverage && cp coverage.info /mnt/coverage/coverage.info
