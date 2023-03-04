1e3cc87c67a9dec6e25d153542e728945de75e02
haritsah <haritsah@qlue.id>
2023-03-05 00:33:12 +0700

    Update onnxrt engine to template class

--- include/cpp_server/onnxrt_engine.hpp
+++ include/cpp_server/onnxrt_engine.hpp
@@ -8,6 +8,7 @@
 #include "base/inference_engine.hpp"
 #include "utils/common.hpp"
 #include "utils/error.hpp"
+#include "cpp_server/onnxrt_helper.hpp"
 
 
 namespace cps_utils = cpp_server::utils;
@@ -16,26 +17,28 @@ namespace cpp_server
 {
     namespace inferencer
     {
-        class ONNXRTEngine : public InferenceEngine
+        template <typename T>
+        class ONNXRTEngine : public InferenceEngine<T>
         {
         public:
             ONNXRTEngine() = default;
             ~ONNXRTEngine() {};
 
-            ONNXRTEngine(const cpp_server::utils::ModelConfig &model_config, const int &batch_size);
+            ONNXRTEngine(const std::string &model_path, const std::vector<cpp_server::utils::ModelConfig> &model_config, const int &batch_size);
 
             ONNXRTEngine(const ONNXRTEngine &engine) = delete;
             ONNXRTEngine &operator=(const ONNXRTEngine &engine);
             ONNXRTEngine(ONNXRTEngine &&engine) = delete;
             ONNXRTEngine &operator=(ONNXRTEngine &&engine);
 
-            cps_utils::Error process(const std::vector<cps_utils::InferenceData<uint8_t>> &infer_data, std::vector<cps_utils::InferenceResult<uint8_t>> &infer_results);
+            cps_utils::Error process(const std::vector<cps_utils::InferenceData<T>> &infer_data, std::vector<cps_utils::InferenceResult<T>> &infer_results);
 
         private:
-            cps_utils::Error validate(const std::vector<cps_utils::InferenceData<uint8_t>> &infer_data);
+            std::unique_ptr<ORTRunner> ort_runner;
 
-        }
+            cps_utils::Error validate(const std::vector<cps_utils::InferenceData<T>> &infer_data);
 
+        };
     }
 }
 
---
+++ src/onnxrt_engine.cpp
@@ -0,0 +1,31 @@
+#include "cpp_server/onnxrt_engine.hpp"
+
+namespace cpp_server
+{
+    namespace inferencer
+    {
+        template <typename T>
+        ONNXRTEngine<T>::ONNXRTEngine(const std::string &model_path, const std::vector<cpp_server::utils::ModelConfig> &model_config, const int &batch_size)
+        {
+            ort_runner.reset(new ORTRunner(model_path, model_config));
+        }
+
+        template <typename T>
+        cps_utils::Error ONNXRTEngine<T>::validate(const std::vector<cps_utils::InferenceData<T>> &infer_data)
+        {
+            return cps_utils::Error::Success;
+        }
+
+        template <typename T>
+        cps_utils::Error ONNXRTEngine<T>::process(const std::vector<cps_utils::InferenceData<T>> &infer_data, std::vector<cps_utils::InferenceResult<T>> &infer_results)
+        {
+            return cps_utils::Error::Success;
+
+        }
+    }
+}
+
+//  https://isocpp.org/wiki/faq/templates#separate-template-class-defn-from-decl
+//  * NOTE: Solve template function linker problem
+
+template class cpp_server::inferencer::ONNXRTEngine<float>;
