063cc0ecb912f8d9a70050186acf10c01bbecfc7
haritsah <haritsah@qlue.id>
2023-03-05 00:32:43 +0700

    Update onnx helper functions to process multiple inputs

--- include/cpp_server/onnxrt_helper.hpp
+++ include/cpp_server/onnxrt_helper.hpp
@@ -5,8 +5,11 @@
 #include <onnxruntime/core/session/onnxruntime_cxx_api.h>
 #include <rapidjson/document.h>
 #include <rapidjson/istreamwrapper.h>
+#include "cpp_server/utils/common.hpp"
+#include "cpp_server/utils/error.hpp"
 #include <fstream>
 
+namespace cps_utils = cpp_server::utils;
 
 namespace cpp_server
 {
@@ -16,13 +19,18 @@ namespace cpp_server
         {
         public:
             ORTRunner() = default;
+            ORTRunner(const std::string &model_path, const std::vector<cps_utils::ModelConfig> &configs);
             ~ORTRunner(){};
+
+            cps_utils::Error process(const std::vector<Ort::Value> &input_tensors, std::vector<Ort::Value> &output_tensors);
 
         private:
             std::unique_ptr<Ort::Session> session_;
+            Ort::Env env_;
+            Ort::RunOptions run_options_;
 
-
-        bool readModelConfigJSON(const std::string &path, rapidjson::Document doc);
+            std::vector<const char*> input_node_names, output_node_names;
+            std::vector<cps_utils::ModelConfig> model_configs;
 
         };
     }
--- src/onnxrt_helper.cpp
+++ src/onnxrt_helper.cpp
@@ -4,17 +4,34 @@ namespace cpp_server
 {
     namespace inferencer
     {
-        bool ORTRunner::readModelConfigJSON(const std::string &path, rapidjson::Document doc)
+        ORTRunner::ORTRunner(const std::string &model_path, const std::vector<cps_utils::ModelConfig> &configs)
         {
-            std::ifstream ifs(path);
-            if (!ifs)
+            session_.reset(new Ort::Session(env_, model_path.c_str(), Ort::SessionOptions{nullptr}));
+            model_configs = configs;
+
+            for(cps_utils::ModelConfig &conf: model_configs)
             {
-                return false;
+                input_node_names.push_back(conf.input_name_.c_str());
+                output_node_names.push_back(conf.output_name_.c_str());
             }
-            rapidjson::IStreamWrapper isw(ifs);
+        }
 
-            return true;
+        cps_utils::Error ORTRunner::process(const std::vector<Ort::Value> &input_tensors, std::vector<Ort::Value> &output_tensors)
+        {
+            if (!session_)
+            {
+                return cps_utils::Error(cps_utils::Error::Code::INTERNAL, "ONNXRT session is not initialized");
+            }
 
+            try
+            {
+                session_->Run(run_options_, input_node_names.data(), input_tensors.data(), input_node_names.size(), output_node_names.data(), output_tensors.data(), output_node_names.size());
+                return cps_utils::Error::Success;
+            }
+            catch (const std::exception& ex)
+            {
+                return cps_utils::Error(cps_utils::Error::Code::INFERENCE_ERROR, ex.what());
+            }
         }
     }
 } // namespace cpp_server
