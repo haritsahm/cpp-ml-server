6a4b235ecf7a6a27353b711d433efc43cb60680a
haritsah <haritsah@qlue.id>
2023-03-06 12:12:29 +0700

    Update cmakelists and test scripts

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmake -DRUN_TESTS=ON  -DENABLE_TRITON=OFF -DCMAKE_BUILD_TYPE=Debug .. && make
-ctest --output-on-failure --progress
+cmake -DRUN_TESTS=ON  -DENABLE_TRITON=OFF -DENABLE_ONNXRT=ON -DCMAKE_BUILD_TYPE=Debug .. && make
+ctest --output-on-failure --progress --verbose
 lcov --capture --directory . --output-file coverage.info
 mkdir -p /mnt/coverage && cp coverage.info /mnt/coverage/coverage.info
