233557d0ffc99ed3547e891323849e9fbe9ca686
haritsah <haritsah@qlue.id>
2023-03-06 12:07:10 +0700

    Update helper functions, methods, and members

--- include/cpp_server/onnxrt_helper.hpp
+++ include/cpp_server/onnxrt_helper.hpp
@@ -15,12 +15,78 @@ namespace cpp_server
 {
     namespace inferencer
     {
+        static std::string getONNXStrElementType(const int &dtype)
+        {
+            switch(dtype)
+            {
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_UNDEFINED
+                case 0:
+                    return "UNDEFINED";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT
+                case 1:
+                    return "FP32";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT8
+                case 2:
+                    return "UINT8";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_INT8
+                case 3:
+                    return "INT8";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT16
+                case 4:
+                    return "UINT16";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_INT16
+                case 5:
+                    return "INT16";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32
+                case 6:
+                    return "INT32";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64
+                case 7:
+                    return "INT64";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING
+                case 8:
+                    return "STRING";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL
+                case 9:
+                    return "BOOL";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16
+                case 10:
+                    return "FP16";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_DOUBLE
+                case 11:
+                    return "FP64";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT32
+                case 12:
+                    return "UINT32";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT64
+                case 13:
+                    return "UINT64";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX64
+                case 14:
+                    return "UNDEFINED";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX128
+                case 15:
+                    return "UNDEFINED";
+                // ONNX_TENSOR_ELEMENT_DATA_TYPE_BFLOAT16
+                case 16:
+                    return "BF16";
+                default:
+                    break;
+            }
+
+            return "UNDEFINED";
+        }
+
         class ORTRunner
         {
         public:
             ORTRunner() = default;
             ORTRunner(const std::string &model_path);
             ~ORTRunner(){};
+
+            cps_utils::Error readModelConfigs();
+            std::vector<cps_utils::ModelConfig> getModelConfigs() { return model_configs_;};
+            bool isValid() {bool val = session_ == nullptr ? false : true; return val;};
 
             cps_utils::Error process(const std::vector<Ort::Value> &input_tensors, std::vector<Ort::Value> &output_tensors);
 
@@ -29,8 +95,8 @@ namespace cpp_server
             Ort::Env env_;
             Ort::RunOptions run_options_;
 
-            std::vector<const char*> input_node_names, output_node_names;
-            std::vector<cps_utils::ModelConfig> model_configs;
+            std::vector<const char*> input_node_names_, output_node_names_;
+            std::vector<cps_utils::ModelConfig> model_configs_;
 
         };
     }
--- src/onnxrt_helper.cpp
+++ src/onnxrt_helper.cpp
@@ -7,13 +7,72 @@ namespace cpp_server
         ORTRunner::ORTRunner(const std::string &model_path)
         {
             session_.reset(new Ort::Session(env_, model_path.c_str(), Ort::SessionOptions{nullptr}));
-            model_configs = configs;
 
-            for(cps_utils::ModelConfig &conf: model_configs)
+            cps_utils::Error p_err;
+            p_err = readModelConfigs();
+            if (!p_err.IsOk())
             {
-                input_node_names.push_back(conf.input_name_.c_str());
-                output_node_names.push_back(conf.output_name_.c_str());
+                session_.reset(nullptr);
             }
+
+        }
+
+        cps_utils::Error ORTRunner::readModelConfigs()
+        {
+            Ort::AllocatorWithDefaultOptions allocator;
+
+            try
+            {
+                for(size_t i = 0; i < session_->GetInputCount(); ++i)
+                {
+                    cps_utils::ModelConfig config_;
+                    auto inputNodeName = session_->GetInputNameAllocated(0, allocator);
+                    config_.input_name_ = std::string(inputNodeName.get());
+
+                    Ort::TypeInfo inputTypeInfo = session_->GetInputTypeInfo(0);
+                    auto inputTensorInfo = inputTypeInfo.GetTensorTypeAndShapeInfo();
+                    config_.input_shape_ = inputTensorInfo.GetShape();
+                    config_.max_batch_size_ = config_.input_shape_[0];
+                    config_.channel_first_ = config_.input_shape_[1] == 3 ? true : false;
+
+                    ONNXTensorElementDataType inputType = inputTensorInfo.GetElementType();
+                    config_.input_datatype_ = getONNXStrElementType(inputType);
+                    config_.input_byte_size_ = cps_utils::vectorProduct(config_.input_shape_) * cps_utils::ElementStrTypeSize[config_.input_datatype_];
+
+                    model_configs_.push_back(config_);
+                    input_node_names_.push_back(model_configs_[i].input_name_.c_str());
+
+                }
+            }
+            catch (std::exception &ex)
+            {
+                return cps_utils::Error(cps_utils::Error::Code::INTERNAL, "Unable to read model input metadata, " + std::string(ex.what()));
+            }
+
+            try
+            {
+                for(size_t i = 0; i < session_->GetOutputCount(); ++i)
+                {
+                    auto outputNodeName = session_->GetOutputNameAllocated(0, allocator);
+                    model_configs_[i].output_name_ = std::string(outputNodeName.get());
+                    output_node_names_.push_back(model_configs_[i].output_name_.c_str());
+
+                    Ort::TypeInfo outputTypeInfo = session_->GetOutputTypeInfo(0);
+                    auto outputTensorInfo = outputTypeInfo.GetTensorTypeAndShapeInfo();
+
+                    model_configs_[i].output_shape_ = outputTensorInfo.GetShape();
+
+                    ONNXTensorElementDataType outputType = outputTensorInfo.GetElementType();
+                    model_configs_[i].output_datatype_ = getONNXStrElementType(outputType);
+                    model_configs_[i].output_byte_size_ = cps_utils::vectorProduct(model_configs_[i].output_shape_) * cps_utils::ElementStrTypeSize[model_configs_[i].input_datatype_];
+                }
+            }
+            catch (std::exception &ex)
+            {
+                return cps_utils::Error(cps_utils::Error::Code::INTERNAL, "Unable to read model output metadata, " + std::string(ex.what()));
+            }
+
+            return cps_utils::Error::Success;
         }
 
         cps_utils::Error ORTRunner::process(const std::vector<Ort::Value> &input_tensors, std::vector<Ort::Value> &output_tensors)
@@ -25,13 +84,14 @@ namespace cpp_server
 
             try
             {
-                session_->Run(run_options_, input_node_names.data(), input_tensors.data(), input_node_names.size(), output_node_names.data(), output_tensors.data(), output_node_names.size());
-                return cps_utils::Error::Success;
+                session_->Run(run_options_, input_node_names_.data(), input_tensors.data(), input_node_names_.size(), output_node_names_.data(), output_tensors.data(), output_node_names_.size());
             }
             catch (const std::exception& ex)
             {
-                return cps_utils::Error(cps_utils::Error::Code::INFERENCE_ERROR, ex.what());
+                return cps_utils::Error(cps_utils::Error::Code::INFERENCE_ERROR, std::string(ex.what()));
             }
+
+            return cps_utils::Error::Success;
         }
     }
 } // namespace cpp_server
